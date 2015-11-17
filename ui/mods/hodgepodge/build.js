372ccbbd5bb25b3b9906cf4cde2555c069d8013a
Justin Love <git@JustinLove.name>
2015-11-17 13:09:01 -0600

    punt anything left into ammo or extra

--- ui/mods/hodgepodge/build.js
+++ ui/mods/hodgepodge/build.js
@@ -24,6 +24,25 @@
     assign(stockbuild[id], id)
   })
 
+  var buildsOnRow = function(build) {
+    var tab = build[0]
+    var row = Math.floor(build[1] / 6) * 6
+    var slots = []
+    for (var i = row;i < row + 6;i++) {
+      slots.push([tab, i])
+    }
+    return slots
+  }
+
+  var buildsInTab = function(build) {
+    var tab = build[0]
+    var slots = []
+    for (var i = 0;i < 18;i++) {
+      slots.push([tab, i])
+    }
+    return slots
+  }
+
   var unassignedUnits = HodgePodge.customUnits
 
   // pass: assign requested slot
@@ -40,16 +59,6 @@
   })
 
   // pass: assign an empty slot on the same row
-  var buildsOnRow = function(build) {
-    var tab = build[0]
-    var row = Math.floor(build[1] / 6) * 6
-    var slots = []
-    for (var i = row;i < row + 6;i++) {
-      slots.push([tab, i])
-    }
-    return slots
-  }
-
   unassignedUnits = unassignedUnits.filter(function(unit) {
     for (var b in unit.preferred_builds) {
       var open = buildsOnRow(unit.preferred_builds[b]).filter(available)
@@ -64,15 +73,6 @@
   })
 
   // pass: assign to any empty slot in a desired tab
-  var buildsInTab = function(build) {
-    var tab = build[0]
-    var slots = []
-    for (var i = 0;i < 18;i++) {
-      slots.push([tab, i])
-    }
-    return slots
-  }
-
   unassignedUnits = unassignedUnits.filter(function(unit) {
     for (var b in unit.preferred_builds) {
       var open = buildsInTab(unit.preferred_builds[b]).filter(available)
@@ -86,12 +86,34 @@
     return true
   })
 
-  // pass: punt into extra tab
-  unassignedUnits.forEach(function(unit, i) {
-    var tab = build[0]
-    unit.assigned_build = ['extra', i]
-    assign(['extra', i], unit.spec_id)
+  // pass: punt into ammo tab
+  unassignedUnits = unassignedUnits.filter(function(unit) {
+    var open = buildsInTab(['ammo', 0]).filter(available)
+    if (open.length > 0) {
+      unit.assigned_build = open[0]
+      assign(open[0], unit.spec_id)
+      return false
+    }
+
+    return true
   })
+
+  // pass: punt into extra tab
+  var n = 0
+  while (unassignedUnits.length > 0) {
+    var tab = 'extra' + n
+    n = n + 1
+    unassignedUnits = unassignedUnits.filter(function(unit) {
+      var open = buildsInTab([tab, 0]).filter(available)
+      if (open.length > 0) {
+        unit.assigned_build = open[0]
+        assign(open[0], unit.spec_id)
+        return false
+      }
+
+      return true
+    })
+  }
 
   var source = Build.HotkeyModel.toString()
   HodgePodge.customUnits.forEach(function(unit) {
