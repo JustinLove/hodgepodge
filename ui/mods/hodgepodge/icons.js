7f1cbe6c2b30e87819adde5a0d2436833e50ca60
Justin Love <git@JustinLove.name>
2016-02-18 19:19:08 -0600

    handle icons internally

--- ui/mods/hodgepodge/icons.js
+++ ui/mods/hodgepodge/icons.js
@@ -1,31 +1,11 @@
 (function() {
-  console.log('hodgepodge icons', !!window.atlasMessage)
-  if (window.atlasMessage) {
-    var removed = _.compact(HodgePodge.removedUnits.map(function(unit) {
-      if (unit.si_name === null) return null
-      return unit.si_name || unit.spec_id.match(/\/(\w+).json/)[1]
-    }))
-    if (removed.length > 0) {
-      atlasMessage.message('icon_atlas', 'release_icons', removed)
-    }
-
-    var added = _.compact(HodgePodge.addedUnits.map(function(unit) {
-      if (unit.si_name === null) return null
-      var si_name = unit.si_name || unit.spec_id.match(/\/(\w+).json/)[1]
-      if (unit.si_fallback) {
-        var o = {}
-        o[si_name] = unit.si_fallback
-        return o
-      } else {
-        return si_name
-      }
-    }))
-    if (added.length > 0) {
-      atlasMessage.message('icon_atlas', 'request_icons', added)
-    }
-
-    if (removed.length > 0 || added.length > 0) {
-      atlasMessage.message('icon_atlas', 'update_and_freeze_icon_changes')
-    }
+  var added = _.compact(HodgePodge.addedUnits.map(function(unit) {
+    if (unit.si_name === null) return null
+    return unit.si_name || unit.spec_id.match(/\/(\w+).json/)[1]
+  }))
+  if (added.length > 0) {
+    model.strategicIcons(_.union(model.strategicIcons(), added).slice(0,315))
+    setTimeout(model.sendIconList, 5000)
+    console.log('hodgepodge added +' + added.length.toString() + ', ' + model.strategicIcons().length.toString() + '/315 icons')
   }
 })()
